b789fd00fb579bd39b7e44e58fd774dfa7a40920
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-12 14:23:55 -0700

    newsletter component

---
+++ client/src/components/Newsletter/Newsletter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NewsletterTab.css';
+
+const NewsletterTab = () => {
+  const handleSubscribe = () => {
+    console.log('Subscribed!');
+  };
+
+  return (
+    <div className="newsletter-tab">
+      <div>
+        <p>Subscribe to our newsletter for the latest updates!</p>
+      </div>
+      <button onClick={handleSubscribe}>Subscribe Now</button>
+    </div>
+  );
+};
+
+export default NewsletterTab;
