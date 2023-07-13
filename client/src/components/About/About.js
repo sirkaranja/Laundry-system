7b09bc706aa8b5c0d412687dbdd10a573726cca1
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-13 05:34:23 -0700

    updated about component with styling

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,14 +5,19 @@ import "../About/about.css"
 export const About = () => {
   return (
     <div className="about-container">
-      <img src={img} alt="About" className="about-image" />
+      <img src={img} alt="About" className="about-image"  />
       <div className="about-text">
-        <h1>Professional Laundry<br/>
-        Experience
+        <h1>Professional <br/> Laundry
+        <br/> Experience
         </h1>
-        <p>lorenwekfnwejnrjnvvm vriovknrv knvnervnkvnlnm vnevnorevioerviern onknvkdvnklvnekvn </p><br />
+        <p id='about-top'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,< br/>
+         sed do eiusmod tempor 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis< br/>
+           nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.< br/>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+             eu fugiat nulla pariatur.</p><br />
 
-        <button>Leran More</button>
+        <button className='custom-button'>Learn More</button>
       </div>
       
     </div>
