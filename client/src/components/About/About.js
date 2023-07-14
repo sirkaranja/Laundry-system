5af2d0929cabded67f99ddbf8dfb9cf8e854e093
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-14 15:52:55 +0300

    added cards and styles that had issues

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,28 +1,54 @@
-import React from 'react'
-import img from '../../assets/about_1.jpg'
-import "../About/about.css"
+import React from 'react';
+import img from '../../assets/about_1.jpg';
+import passionate from '../../assets/passionate.jpg'; // Import your service image here
+import "../About/about.css";
 
-export const About = () => {
+const About = () => {
   return (
-    <div className="about-container">
-      <img src={img} alt="About" className="about-image"  />
-      <div className="about-text">
-        <h1>Professional <br/> Laundry
-        <br/> Experience
-        </h1>
-        <p id='about-top'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,< br/>
-         sed do eiusmod tempor 
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis< br/>
-           nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.< br/>
+    <>
+      <div className="about-container">
+        <img src={img} alt="About" className="about-image" />
+        <div className="about-text">
+          <h1>Professional <br /> Laundry
+            <br /> Experience
+          </h1>
+          <p id='about-top'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
+            sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis<br />
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br />
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-             eu fugiat nulla pariatur.</p><br />
+            eu fugiat nulla pariatur.</p><br />
 
-        <button className='custom-button'>Learn More</button>
-      </div>  
-    </div>
-    
-    
-  )
-}
+          <button className='custom-button'>Learn More</button>
+        </div>
+      </div>
+      <div>
+        <h2>Our Laundry <br/>Services</h2>
+      </div>
+        
+      <div className="services-section">
+        
+        <img src={passionate} alt="Service" className="service-image" /> 
+        <div className="services-cards">
+          <div className="service-card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
+              sed do eiusmod tempor
+              incididunt ut labore</p>
+          </div>
+          <div className="service-card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
+              sed do eiusmod tempor
+              incididunt ut labore</p>
+          </div>
+          <div className="service-card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br />
+              sed do eiusmod tempor
+              incididunt ut labore</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
