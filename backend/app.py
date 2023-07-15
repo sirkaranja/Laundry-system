82113ba77a2eaa4558ea26e5d87480e334fd3b9f
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-15 16:10:04 +0300

    app configuration

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///laundry.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+@app.route('/')
+def index():
+    return "<h3>Dashboard</h3>"
+
+
+
+if __name__ =='__main__':
+    app.run(port=5555)
