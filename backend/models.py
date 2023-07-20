682a8ba16b7409dd9dbeb9838ac22b94749ad476
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-20 23:16:17 +0300

    updated foreign key to customers.id

--- backend/models.py
+++ backend/models.py
@@ -1,38 +1,31 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 db = SQLAlchemy()
 
 class Customer(db.Model):
-    __tablename__='customers'
+    __tablename__ = 'customers'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    
 
 class Order(db.Model):
-    __tablename__='orders'
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
     status = db.Column(db.String(20), nullable=False)  # 'pending', 'processing', 'ready to deliver'
 
 class Expense(db.Model):
-    __tablename__='expenses'
+    __tablename__ = 'expenses'
     id = db.Column(db.Integer, primary_key=True)
 
-
 class Service(db.Model):
-    __tablename__='services'
+    __tablename__ = 'services'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
-
 class PaymentDetail(db.Model):
-    __tablename__='paymentdetails'
+    __tablename__ = 'paymentdetails'
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
     is_dryer = db.Column(db.Boolean, default=False)
     extra_care_charge = db.Column(db.Float, default=0.0)
     delivery_charge = db.Column(db.Float, default=0.0)
-
